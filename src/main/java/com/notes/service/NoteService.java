df0c25fbd15b87d25c389ef91c5b9016020fe28b
gitanitraj <anitra.com@gmail.com>
2026-04-30 16:12:25 -0400

    Added NoteService Class - 1st Layer

--- src/main/java/com/notes/service/NoteService.java
+++ src/main/java/com/notes/service/NoteService.java
@@ -1,1 +1,25 @@
 package com.notes.service;
+
+import com.notes.model.Note;
+import com.notes.utils.FileUtils;
+import com.notes.utils.NoteFormatter;
+
+public class NoteService {
+
+    private static final String NOTES_FOLDER = "notes";
+
+    public void createNote(Note note, String filename) {
+        // Format note into YAML text
+    String formatted = NoteFormatter.formatNote(note);
+
+    // Build full file path inside ~/.notes/notes/
+    String filePath = FileUtils.buildNoteFilePath(NOTES_FOLDER + "/" + filename);
+
+    // Write file
+    FileUtils.writeFile(filePath, formatted);
+
+    System.out.println("Note created at: " + filePath);
+
+    }
+
+}
