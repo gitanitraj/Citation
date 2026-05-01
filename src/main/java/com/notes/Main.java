e5a44dc74d7965b12587e621050cf5cef414a399
gitanitraj <anitra.com@gmail.com>
2026-05-01 14:26:47 -0400

    Updated Main

--- src/main/java/com/notes/Main.java
+++ src/main/java/com/notes/Main.java
@@ -3,6 +3,8 @@ package com.notes;
 public class Main {
     public static void main(String[] args) {
 
+        args = new String[]{"list"}; //TEMP DEBUG
+
         String dir = Config.ensureNotesDirectoryExists();
         System.out.println("Notes directory: " + dir);
 
